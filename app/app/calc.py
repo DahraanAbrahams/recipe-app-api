259d3a5d624ba07d25b69a3092f918373c27cc59
Dahraan Abrahams <dahraan.abrahams@gmail.com>
2024-01-22 18:14:54 +0200

    Fix calc.py

--- app/app/calc.py
+++
@@ -1,9 +0,0 @@
-# """ noqa
-# Calculator functions
-# """
-# def add(x, y): noqa
-#     """Add x and y and return result"""
-#     return x + y
-# def subtract(x, y): noqa
-#     """Subtract x and y and return result"""
-#     return y - x
